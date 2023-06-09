76d04f8cfcbfd7bd37d8c88746f3bb01462b29ae
chihiro0401 <129600067+chihiro0401@users.noreply.github.com>
2023-06-09 16:30:00 +0000

    　コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ #会員側のルーティング
+  devise_for :users
+  
+  #管理者側のルーティング
+  # URL /admin/sign_in ...
+  devise_for :admin, controllers: {
+  #ログイン、ログアウト関連
+  sessions: "admin/sessions",
+}
+ #トップ
+  get 'homes/top'
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 end
