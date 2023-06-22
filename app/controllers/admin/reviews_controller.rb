36b0098909d09cc75b93a263654a687e5a718484
chihiro0401 <129600067+chihiro0401@users.noreply.github.com>
2023-06-22 12:01:22 +0000

    レビュー削除追加

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,23 +1,25 @@
 class Admin::ReviewsController < ApplicationController
   # before_action :authenticate_admin!
-  
-  
+
+
   def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
     @reviews = @user.reviews
-end
+  end
 
   def destroy
-    @reviews = Review.find(params[:id])
-    @reviews.destroy
+    @review = Review.find(params[:id])
+    @review.destroy
     redirect_to request.referer
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :is_deleted)
+  end
+end
